891c52563955b2306972e0e303f7df4159cfeeac
Ahmed Mamdouh <amamdouhdeveloper@gmail.com>
2026-09-12 19:59:11 +0300

    langgraph nodes implementation.

--- nodes.py
+++ nodes.py
@@ -1,5 +1,36 @@
 # This file will hold the implementation of our langgraph nodes.
 
+from dotenv import load_dotenv
 
+# MessagesState is a simple object which has the dictionary of the key messages.
+# And that messages is going to be a list of messages.
+# So this is going to keep track on the state on all of the messages back in between our agent, the human message and the AI message.
+from langgraph.graph import MessagesState
 
+# This is a node.
+# It's going to check the last message between the agent and human.
+# And if that last message is an AI message that has a valid tool call, it's going to go and execute that
+# tool code, assuming that tools is initiated with the tool node object.
+from langgraph.prebuilt import ToolNode
 
+from react import llm, tools
+
+load_dotenv()
+
+SYSYEM_MESSAGE="""
+You are a helpful assistant that can use tools to answer questions.
+"""
+
+# This is the agent_reason node depicted in the infrastructure image that will receive our message and decide wether to answer or Act(call a tool).
+def run_agent_reasoning(state: MessagesState) -> MessagesState:
+    """
+    Run the agent reasoning node.
+    """
+    # Note, all the information will be in the *state["messages"] over here, the first iteration will have the human message, and the second iteration will have the human and the AI messages and so on.
+    # So the state will be preserved here in the *state["messages"]
+    response = llm.invoke([{"role": "system", "content": SYSYEM_MESSAGE}, *state["messages"]])
+
+    return {"messages": [response]} # So after the llm is invoked we need to update our state, that's why we appended the response to the messages key and returned it.
+
+# We define the tool node with the relevant tools, this is depicted as Act node in the infrastructure image.
+tool_node = ToolNode(tools)
--- react.py
+++ react.py
@@ -1,4 +1,26 @@
+from dotenv import load_dotenv
+from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+from langchain_ollama import ChatOllama
+
+load_dotenv()
+
+
 # This file will hold our reasoning engine.
 
+@tool
+def triple(num: float) -> float:
+    """
+    param num: a number to triple
+    returns: the triple of the input number
+    """
 
+    return float(num) * 3
+
+
+tools = [TavilySearch(max_results = 1), triple]
+
+# There are many ways to define function calling to LLM, one via ReAct prompt and another one via LLM because LLMs has evolved nowadays, and bind_tools function let us define function calling to LLM, and this is ofcourse based on the LLM vendor.
+llm = ChatOllama(temperature=0, model="qwen3:8b").bind_tools(tools) # To review bind_tools function review the lesson 93. [Hands On] Coding the Agent's Brain: Implementing the ReAct Runnable.
 
